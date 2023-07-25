ae453eabf5bd8c618cd6c1d4ef80309dbd7d854b
Haseeb Burki <hzburki.hzb@gmail.com>
2023-07-25 23:33:54 +0500

    added mdx package and npm installed but not sure if I am going to use it or not.

---
+++ app/about-me/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutMe() {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-semibold font-nunito">About Me</h1>
+    </div>
+  );
+}
---
+++ app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-semibold font-nunito">Blog</h1>
+    </div>
+  );
+}
