5da0a6c5c6cb97dce0ab9441fb3827d34c039205
Haseeb Burki <hzburki.hzb@gmail.com>
2023-07-26 23:45:17 +0500

    configured contentLayer but its untested

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Personal Blog
 
-## VSCode Extensions
+## VSCode Extensions & Third Party Packages
 
-1. MDX Preview
+1. [MDX Preview](https://marketplace.visualstudio.com/items?itemName=unifiedjs.vscode-mdx)
+2. [Content Layer](https://www.contentlayer.dev/)
 ## Tech Stack
 
 1. NextJS (ReactJS)
--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
+import { withContentlayer } from 'next-contentlayer'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: true,
+  reactStrictMode: true,
   experimental: {
     mdxRs: true,
   },
 }
 
 const withMDX = require('@next/mdx')()
-module.exports = withMDX(nextConfig)
+module.exports = withContentlayer(withMDX(nextConfig)) 
