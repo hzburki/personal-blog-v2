abcf9356b8faafeefcc9dac6edb38371f49a9e00
Haseeb Burki <hzburki.hzb@gmail.com>
2023-08-01 20:57:00 +0500

    main page header and footer done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="p-4">
-      <h1 className="text-3xl font-semibold font-nunito">Home</h1>
-    </div>
+    <main>
+      <header className="px-6 py-4 max-w-4xl mx-auto flex flex-col sm:flex-row sm:justify-between">
+        {/* Left Container */}
+        <div className="flex border text-3xl justify-center mb-2 sm:mb-0 sm:justify-start">
+          <Link
+            href="/"
+            className="hover:underline hover:font-bold font-semibold"
+          >
+            hzburki.com
+          </Link>
+        </div>
+
+        {/* Right Container */}
+        <div className="border text-2xl flex flex-row justify-evenly gap-4">
+          <Link className="hover:underline hover:font-bold" href="/about-me">
+            About
+          </Link>
+          <Link className="hover:underline hover:font-bold" href="/blog">
+            Blog
+          </Link>
+        </div>
+      </header>
+      <footer className="text-center py-2">Made with NextJS + Markdown</footer>
+    </main>
   );
 }
